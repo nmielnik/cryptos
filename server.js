b5163a65de782912f6fc5aa0358cfdc98ed71de9
Nate Mielnik <nathan@outlook.com>
2017-05-23 02:16:34 -0400

    add /prices to get all current cryptocompare prices

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const _ = require('lodash');
+const Promise = require('bluebird');
+const rest = require('rest');
+const mime = require('rest/interceptor/mime');
+const allCoins = require('./lib/coin-names').all;
+
+const client = rest.wrap(mime);
+
 var express = require("express");
 var app = express();
 var router = express.Router();
@@ -24,6 +32,30 @@ router.get("/info", function(req, res) {
 	});
 });
 
+router.get("/prices", function (req, res) {
+	const theCoins = {};
+
+	client({ path: 'https://www.cryptocompare.com/api/data/coinlist/' }).then((respData) => {
+	    const allCoinNames = Object.keys(respData.entity.Data);
+	    // Split list of all coins into chunks of 10 coins each for querying
+	    const chunks = _.chunk(allCoinNames, 10);
+
+	    return Promise.all(chunks.map((set) => {
+			return client({ path: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${set.join(',')}&tsyms=BTC`})
+		}))
+		.then((parts) => {
+			parts.forEach((response) => {
+				const coinData = response.entity;
+				Object.keys(coinData).forEach((coinName) => {
+					theCoins[coinName] = coinData[coinName];
+				})
+			});
+			res.json(theCoins);
+		});
+	}).catch((error) => {
+		res.status(500).send(error);
+	});
+})
 
 app.use("/", router);
 

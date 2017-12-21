f69c8bf88c44940aab05ca7421cab00a4542e617
Richard Adjogah <richard@webs.com>
2017-12-21 11:42:19 -0500

    Add trading logic, support for custom trade rules

---
+++ trade-rules.js
@@ -0,0 +1,8 @@
+module.exports = {
+	totalInvestmentSize: 0.01,
+	maxCoins: 10,
+	targetGrowth: 3,
+	thresholdPct: 10,
+	thresholdMins: 15,
+	cooldown: 360, // 12hr; measured in number of iterations
+};
